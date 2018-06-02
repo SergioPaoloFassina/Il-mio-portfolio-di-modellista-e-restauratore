d51361a1117ab3da6157b89ec521a0b982b1cf45
SergioPaoloFassina <39835646+SergioPaoloFassina@users.noreply.github.com>
2018-06-02 21:07:01 +0200

    Primo caricamento

---
+++ js/main.js
@@ -0,0 +1,84 @@
+(function($) {
+
+	"use strict";
+
+		// Sidebar link activation
+    $('.navigation').singlePageNav({
+        currentClass : 'active'
+    });
+
+
+    $('.toggle-menu').click(function(){
+        $('.responsive-menu').stop(true,true).slideToggle();
+        return false;
+    });
+
+		// Super gallery
+		lightGallery(document.getElementById('lightgallery'), {
+				altRegExp: v => v.substr(v.lastIndexOf('/') + 1).replace(/[-_]/g, ' ').replace(/.(JPEG|JPG|PNG)/gi, '')
+		})
+
+    /*==================================================================
+    [ Validate after type ]*/
+    $('.validate-input .input').each(function(){
+        $(this).on('blur', function(){
+            if(validate(this) == false){
+                showValidate(this);
+            }
+            else {
+                $(this).parent().addClass('true-validate');
+            }
+        })
+    })
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+           $(this).parent().removeClass('true-validate');
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+})(jQuery);
