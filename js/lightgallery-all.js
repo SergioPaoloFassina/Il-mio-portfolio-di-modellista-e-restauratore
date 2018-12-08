c48e6a59960d6fff659cf3cbed443f3b3951516c
SergioPaoloFassina <39835646+SergioPaoloFassina@users.noreply.github.com>
2018-12-08 12:13:57 +0100

    lg-sub-html will hide-with-controls

--- js/lightgallery-all.js
+++ js/lightgallery-all.js
@@ -305,7 +305,7 @@
         }
 
         if (this.s.appendSubHtmlTo === '.lg-sub-html') {
-            subHtmlCont = '<div class="lg-sub-html"></div>';
+            subHtmlCont = '<div class="lg-sub-html hide-with-controls"></div>';
         }
 
         template = '<div class="lg-outer ' + this.s.addClass + ' ' + this.s.startClass + '">' +
