7d02cf61027bd4cfcac2fd89fe655aa9265e8f61
Fred's GitHub <stopchemtrailsfred@gmail.com>
2019-10-06 14:20:45 +0200

    Fix banner

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
     "use strict";
     
-    var intro = document.querySelector('.banner');
-    var introPlayer = document.querySelector('.banner__video');
+    var intro = document.querySelector('.banner-bg');
+    var introPlayer = document.querySelector('.banner-video');
 
     if (/iPad|iPhone|iPod/.test(navigator.platform)) {
         intro.style.backgroundImage = 'url("' + introPlayer.poster + '")';
